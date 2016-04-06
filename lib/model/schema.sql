16b4768d8cd07521022a83417bdff04037c7a78b
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-04-06 10:37:22 -0700

    Update schema with a lot more structured data
    As suggested by Dr. Ami Bhatt

--- lib/model/schema.sql
+++ lib/model/schema.sql
@@ -6,10 +6,20 @@ drop view if exists patient_info ;
 drop view if exists last_gender_record ;
 drop view if exists last_weight_record ;
 drop view if exists last_height_record ;
+drop view if exists last_diagnosis_record ;
+drop view if exists last_body_temperature_record ;
+drop view if exists last_heart_rate_record ;
+drop view if exists last_blood_pressure_record ;
+drop view if exists last_labresults_record ;
 
 drop table if exists gender_record cascade ;
 drop table if exists weight_record cascade ;
 drop table if exists height_record cascade ;
+drop table if exists diagnosis_record cascade ;
+drop table if exists body_temperature_record cascade ;
+drop table if exists heart_rate_record cascade ;
+drop table if exists blood_pressure_record cascade ;
+drop table if exists labresults_record cascade ;
 drop table if exists medical_record cascade ;
 drop table if exists patient_acl cascade ;
 drop table if exists doctor cascade ;
@@ -53,7 +63,8 @@ create table medical_record (
     patient_id integer not null,
     capture_time datetime not null,
     collect_time datetime null default null,
-    record_type enum('diagnosis', 'xray', 'catscan', 'dnasequence', 'gender', 'weight', 'height', 'other') not null default 'other',
+    record_type enum('diagnosis', 'xray', 'catscan', 'dnasequence', 'gender', 'weight', 'height', 'other', 'body_temperature', 'heart_rate', 'blood_pressure', 'labresults')
+    not null default 'other',
     picture_url varchar(2048) null default null,
 
     foreign key (patient_id) references patient(id) on update restrict on delete cascade,
@@ -84,6 +95,46 @@ create table height_record (
     foreign key (id) references medical_record(id) on update restrict on delete cascade
 ) collate utf8_bin ;
 
+create table diagnosis_record (
+    id integer primary key,
+    cancer_type enum('breast', 'ovarian', 'other') not null default 'other',
+    stage enum('I', 'II', 'III', 'IV') null,
+    substage enum('a', 'b', 'c') null,
+
+    foreign key (id) references medical_record(id) on update restrict on delete cascade
+) collate utf8_bin ;
+
+create table body_temperature_record (
+    id integer primary key,
+    temp_C double,
+
+    foreign key (id) references medical_record(id) on update restrict on delete cascade
+) collate utf8_bin ;
+
+create table heart_rate_record (
+    id integer primary key,
+    heart_rate_bpm double,
+
+    foreign key (id) references medical_record(id) on update restrict on delete cascade
+) collate utf8_bin ;
+
+create table blood_pressure_record (
+    id integer primary key,
+    blood_pressure_mmHg double,
+
+    foreign key (id) references medical_record(id) on update restrict on delete cascade
+) collate utf8_bin ;
+
+create table labresults_record (
+    id integer primary key,
+    white_blood_cell_count double,
+    hematocrit_percent double,
+    hemoglobin double,
+    platelet_count double,
+
+    foreign key (id) references medical_record(id) on update restrict on delete cascade
+) collate utf8_bin ;
+
 create view last_gender_record(id, patient_id, collect_time, gender, chromosomes, other_gender)
 as select gr.id, mr.patient_id, mr.collect_time, gr.gender, gr.chromosomes, gr.other_gender
 from gender_record gr, medical_record mr where gr.id = mr.id and mr.record_type = 'gender' and
@@ -98,6 +149,31 @@ create view last_height_record(id, patient_id, collect_time, height_cms)
 as select hr.id, mr.patient_id, mr.collect_time, hr.height_cms
 from height_record hr, medical_record mr where hr.id = mr.id and mr.record_type = 'height' and
 mr.capture_time = (select max(mr2.capture_time) from medical_record mr2 where mr2.record_type = 'height' and mr2.patient_id = mr.patient_id);
+
+create view last_diagnosis_record(id, patient_id, collect_time, cancer_type, stage, substage)
+as select dr.id, mr.patient_id, mr.collect_time, dr.cancer_type, dr.stage, dr.substage
+from diagnosis_record dr, medical_record mr where dr.id = mr.id and mr.record_type = 'diagnosis' and
+mr.capture_time = (select max(mr2.capture_time) from medical_record mr2 where mr2.record_type = 'diagnosis' and mr2.patient_id = mr.patient_id);
+
+create view last_body_temperature_record(id, patient_id, collect_time, temp_C)
+as select btr.id, mr.patient_id, mr.collect_time, btr.temp_C
+from body_temperature_record btr, medical_record mr where btr.id = mr.id and mr.record_type = 'body_temperature' and
+mr.capture_time = (select max(mr2.capture_time) from medical_record mr2 where mr2.record_type = 'body_temperature' and mr2.patient_id = mr.patient_id);
+
+create view last_heart_rate_record(id, patient_id, collect_time, heart_rate_bpm)
+as select hrr.id, mr.patient_id, mr.collect_time, hrr.heart_rate_bpm
+from heart_rate_record hrr, medical_record mr where hrr.id = mr.id and mr.record_type = 'heart_rate' and
+mr.capture_time = (select max(mr2.capture_time) from medical_record mr2 where mr2.record_type = 'heart_rate' and mr2.patient_id = mr.patient_id);
+
+create view last_blood_pressure_record(id, patient_id, collect_time, blood_pressure_mmHg)
+as select bpr.id, mr.patient_id, mr.collect_time, bpr.blood_pressure_mmHg
+from blood_pressure_record bpr, medical_record mr where bpr.id = mr.id and mr.record_type = 'blood_pressure' and
+mr.capture_time = (select max(mr2.capture_time) from medical_record mr2 where mr2.record_type = 'blood_pressure' and mr2.patient_id = mr.patient_id);
+
+create view last_labresults_record(id, patient_id, collect_time, white_blood_cell_count, hematocrit_percent, hemoglobin, platelet_count)
+as select lr.id, mr.patient_id, mr.collect_time, lr.white_blood_cell_count, lr.hematocrit_percent, lr.hemoglobin, lr.platelet_count
+from labresults_record lr, medical_record mr where lr.id = mr.id and mr.record_type = 'labresults' and
+mr.capture_time = (select max(mr2.capture_time) from medical_record mr2 where mr2.record_type = 'labresults' and mr2.patient_id = mr.patient_id);
 
 create view patient_info(id, omlet_account, full_name, nick_name, date_of_birth, gender, weight, height)
 as select p.id, p.omlet_account, p.full_name, p.nick_name, p.date_of_birth, gr.gender, wr.weight_kgs, hr.height_cms
